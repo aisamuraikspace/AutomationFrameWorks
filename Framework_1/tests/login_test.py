b5b22f72e4855006c02b2ef4af984fa4d90ff68a
mlively <mlively@gaig.com>
2018-12-30 08:42:08 -0500

    Added sceehshot taking with correct test name on failure.

--- Framework_1/tests/login_test.py
+++ Framework_1/tests/login_test.py
@@ -1,7 +1,9 @@
+import allure
 from selenium import webdriver
 import pytest
 import time
 import unittest
+import moment
 import HtmlTestRunner
 from Framework_1.utils import utils as utils
 from Framework_1.pages.loginPage import LoginPage
@@ -22,10 +24,34 @@ class TestLogin():
         assert x=="OrangeHRM"
 
     def test_logout(self):
-        driver = self.driver
+        try:
+            driver = self.driver
+            homepage = HomePage(driver)
+            homepage.click_welcome()
+            homepage.click_logout()
+            x = driver.title
+            #assert x == "OrangeHRM"
+            assert x == "HRM"
+            print("test completed")
 
-        homepage = HomePage(driver)
-        homepage.click_welcome()
-        homepage.click_logout()
-        print("test completed")
+        except AssertionError as error:
+            print("Assertion error")
+            print(error)
+            currTime = moment.now().strftime("%d-%m-%Y_%H-%M-%S")
+            realName = utils.whoami()
+            screenShotName = realName + "_" + currTime
+
+            allure.attach(self.driver.get_screenshot_as_png(), name=screenShotName,attachment_type=allure.attachment_type.PNG)
+            driver.get_screenshot_as_file("C:/Users/mlively/PycharmProjects/AutomationFrameWorks/Framework_1/screenshots/"+screenShotName+".png")
+            raise
+
+        except:
+            print("There was an exception")
+            raise
+
+        else:
+            print("No Error Occured")
+
+        finally:
+            print("run code or perform task regardless of error")
 
